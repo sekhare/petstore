8792b39e36f64fabaf970b6d96a54088197587ae
sekhar <sekhare@hotmail.com>
2017-05-03 09:00:46 +0530

    created the controller and model class for pet

---
+++ src/main/java/com/pet/controller/PetController.java
@@ -0,0 +1,48 @@
+package com.pet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pet.model.Pet;
+
+@RestController
+@RequestMapping("/pet")
+public class PetController {
+
+
+    @RequestMapping(value="/addpet",method=RequestMethod.POST)
+	public Pet create()
+	{
+		return null;
+	}
+    
+    @RequestMapping(value="/updatePet",method=RequestMethod.PATCH)
+	public Pet update()
+	{
+		return null;
+	}
+    
+    
+    @RequestMapping(value="/deletePet",method=RequestMethod.GET)
+	public Pet deete()
+	{
+		return null;
+	}
+    
+    @RequestMapping(value="/getPetById",method=RequestMethod.GET)
+	public Pet read()
+	{
+		return null;
+	}
+    @RequestMapping(value="/getAllPets",method=RequestMethod.GET)
+	public Pet readAll()
+	{
+		return null;
+	}
+
+    
+    
+    
+    
+}
---
+++ src/main/java/com/pet/model/Pet.java
@@ -0,0 +1,39 @@
+package com.pet.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.test.annotation.Commit;
+
+
+
+public class Pet {
+	
+	
+	private int id;
+	
+	private String petName;
+	private int petAge;
+	private double price;
+	
+	public String getPetName() {
+		return petName;
+	}
+	public void setPetName(String petName) {
+		this.petName = petName;
+	}
+	public int getPetAge() {
+		return petAge;
+	}
+	public void setPetAge(int petAge) {
+		this.petAge = petAge;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	 
+	
+	
+
+}
